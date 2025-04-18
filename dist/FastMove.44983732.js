f58dc288240b57f5af6d2f1ace6fb580c08a8f71
o.ozerov.dev@gmail.com <o.ozerov.dev@gmail.com>
2025-04-18 14:24:09 +0300

    Добавил билд в docs для GitHub Pages

--- dist/FastMove.44983732.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const form = document.querySelector('.contacts__form');
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    form.reset();
-});
-
-//# sourceMappingURL=FastMove.44983732.js.map
---
+++ docs/FastMove.f88e5fc8.js
@@ -0,0 +1,2 @@
+"use strict";let form=document.querySelector(".contacts__form");form.addEventListener("submit",e=>{e.preventDefault(),form.reset()});
+//# sourceMappingURL=FastMove.f88e5fc8.js.map
